e84c656806d76663cfba0a19af8fcab29c86082e
bjLiu <15890940381@163.com>
2018-09-03 10:12:16 +0800

    add basic stuctrue by bjLiu

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+      {{title}}
+    </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      title: '首页'
+    }
+  }
+} 
+</script>
+
+<style>
+
+</style>
